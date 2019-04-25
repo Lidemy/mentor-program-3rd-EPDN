081c716282d19c3b2fd357a4f2422367e568dc36
EP <epdn@EPs-MacBook.attlocal.net>
2019-04-25 15:56:17 -0700

    week2

--- homeworks/week2/hw3.js
+++ homeworks/week2/hw3.js
@@ -1,5 +1,8 @@
 function reverse(str) {
-  return str;
+  let newStr = '';
+  for (let i = str.length - 1; i >= 0; i -= 1) {
+    newStr += str[i];
+  }
+  return newStr;
 }
-
 reverse('hello');
--- homeworks/week2/hw5.js
+++ homeworks/week2/hw5.js
@@ -1,10 +1,27 @@
-function join(str, concatStr) {
-  return concatStr;
+function join(arr, concatStr) {
+  let r1 = '';
+  for (let i = 0; i < arr.length; i += 1) {
+    if (i === arr.length - 1) {
+      r1 += arr[i];
+    } else {
+      r1 += arr[i] + concatStr;
+    }
+  }
+  return r1;
 }
+
+console.log(join([1, 2, 3], ''));
+console.log(join(['a', 'b', 'c'], '!'));
 
 function repeat(str, times) {
-  return times;
+  let r2 = '';
+  for (let i = 1; i <= times; i += 1) {
+    r2 += str;
+  }
+
+  return r2;
 }
 
-console.log(join('a', '!'));
-console.log(repeat('a', 5));
+
+console.log(repeat('EP', 5));
+console.log(repeat('yoyo', 2));
